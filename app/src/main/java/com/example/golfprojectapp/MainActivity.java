1ca7a757bc48218b3e5bb4a538b51c387593a72f
Morten Olsen <40779401+Min222@users.noreply.github.com>
2019-11-14 12:05:24 +0100

    Added some views

--- app/src/main/java/com/example/golfprojectapp/MainActivity.java
+++ app/src/main/java/com/example/golfprojectapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.golfprojectapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,20 +20,32 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MyActivity";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
+    private Button btnDatePicker, btnTimePicker;
+    private EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.main_tw);
+        textView = findViewById(R.id.list_tw);
         textView.setText("Scores:\n\n");
+
+        btnDatePicker = findViewById(R.id.btn_date);
+        btnTimePicker = findViewById(R.id.btn_time);
+        txtDate = findViewById(R.id.in_date);
+        txtTime = findViewById(R.id.in_time);
+
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
 
         firebase();
     }
@@ -43,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(dataSnapshot.getKey());
 
                 GolfScore golfScore = dataSnapshot.getValue(GolfScore.class);
-                textView.append("Time: " + new Date(golfScore.getTime()) + "\nColor: " + golfScore.getColor() + "\n\n");
+//                textView.append("Time: " + new Date(golfScore.getTime()) + "\nColor: " + golfScore.getColor() + "\n\n");
+                textView.append("Time: " + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(golfScore.getTime())) + "\nColor: " + golfScore.getColor() + "\n\n");
 
                 System.out.println("Time: " + golfScore.getTime());
                 System.out.println("Color: " + golfScore.getColor());
@@ -70,5 +90,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnDatePicker) {
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                }
+            }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+
+        if (v == btnTimePicker) {
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    txtTime.setText(hourOfDay + ":" + minute);
+                }
+            }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
     }
 }
